c0507d15ca94c1fca16707846886be28e8429938
Nino Sopoian <123562974+Nino5982@users.noreply.github.com>
2023-09-07 13:55:43 +0400

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,39 @@
+let acordionWrap = document.querySelectorAll(".step");
+// for (let item of acordionWrap) {
+//   item.addEventListener("click", function (e) {
+//     this.classList.toggle('active');
+//     // e.currentTarget.classList.toggle('active')
+//   });
+// }
+
+acordionWrap.forEach(function (elemenet) {
+  elemenet.addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+});
+
+function getinformation() {
+  let requist = new XMLHttpRequest();
+  // Request.open("method", "url");
+  requist.open("GET", "https://reqres.in/api/unknown");
+  requist.addEventListener("load", function () {
+    let mosuliinfoText = this.responseText;
+    let mosuliinfoJs = JSON.parse(mosuliinfoText);
+    console.log(mosuliinfoJs);
+
+    let ul = document.getElementById("ul");
+    mosuliinfoJs.data.forEach((element2) => {
+      let li = document.createElement("li");
+      let liStyle = li;
+      liStyle.style.color = element2.color;
+      // li.innerText = element2.name + "_____" + element2.color;
+      li.innerText = `${element2.name}______${element2.color}`;
+      ul.appendChild(li);
+    });
+    document.getElementById("api-wraper").appendChild(ul);
+  });
+  requist.send();
+}
+getinformation();
+
+// 25wutze
